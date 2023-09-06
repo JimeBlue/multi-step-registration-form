5c377cb0579bc6fc5ad594d82adb0cdf5fdd099d
Jimena Miguez <j.miguez@airlst.com>
2023-09-06 18:36:28 +0200

    Add skeleton to form component

--- components/form.vue
+++ components/form.vue
@@ -1,3 +1,54 @@
 <template>
-  <p>here comes the form</p>
+  <section>
+    <div v-if="step === 1">
+      <h2>Personal Information</h2>
+      <input v-model="form.name" placeholder="Name" />
+      <input v-model="form.email" placeholder="Email" />
+      <button @click="nextStep">Next</button>
+    </div>
+    <div v-if="step === 2">
+      <h2>Select Your Plan</h2>
+
+      <button @click="prevStep">Previous</button>
+      <button @click="nextStep">Next</button>
+    </div>
+    <div v-if="step === 3">
+      <h2>Pick Add-ons</h2>
+
+      <button @click="prevStep">Previous</button>
+      <button @click="nextStep">Next</button>
+    </div>
+    <div v-if="step === 4">
+      <h2>Summary</h2>
+
+      <button @click="prevStep">Previous</button>
+      <button @click="submitForm">Submit</button>
+    </div>
+  </section>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      step: 1,
+      form: {
+        name: "",
+        email: "",
+        // Add other form fields here
+      },
+    };
+  },
+  methods: {
+    nextStep() {
+      // Add validation logic here using VeeValidate
+      this.step++;
+    },
+    prevStep() {
+      this.step--;
+    },
+    submitForm() {
+      // Submit the form data to Firebase
+    },
+  },
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <p class="text-blue-500 font-bold">Multi Step Registration Form</p>
+  <section>
+    <h2 class="text-blue-500 font-bold">Multi Step Registration Form</h2>
+    <Form />
+  </section>
 </template>
 
 <script>
